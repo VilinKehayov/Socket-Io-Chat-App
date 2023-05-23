9ae05dc361f37461b5da533d79ea9a72076d1f5a
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-05-23 16:04:04 +0300

    useState hook, joinRoom function, socket event.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import "./App.css";
-import io from "socket.io-client"
+import io from "socket.io-client";
+import { useState } from "react";
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App">
-    <h3>Join a room</h3>
-    <input type="text" placeholder="Vilin.."></input>
-    <input type="text" placeholder="Room ID..."></input>
-  </div>;
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+  return (
+    <div className="App">
+      <h3>Join a room</h3>
+      <input
+        type="text"
+        placeholder="Vilin.."
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      ></input>
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      ></input>
+      <button onClick={joinRoom}>Join</button>
+    </div>
+  );
 }
 
 export default App;
