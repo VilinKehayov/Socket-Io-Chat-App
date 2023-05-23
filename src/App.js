df0f87ac877f96003e27e3f2a4d0e931b72e40b3
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-05-23 15:40:24 +0300

    Installed the socket-io-client, made the connect..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import io from "socket.io-client"
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h3>Join a room</h3>
+    <input type="text" placeholder="Vilin.."></input>
+    <input type="text" placeholder="Room ID..."></input>
+  </div>;
 }
 
 export default App;
