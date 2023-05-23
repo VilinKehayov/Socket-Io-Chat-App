28e86139299f1a95dff8c27f341950e032f63fbd
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-05-23 12:39:36 +0300

    I created the server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import express from "express"
-import http from "http"
-import cors from "cors"
+import express from "express";
+import http from "http";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log("Server running");
+});
