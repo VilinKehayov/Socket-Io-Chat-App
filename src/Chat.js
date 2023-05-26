93237eba99d92fac3ab42a35f6d04b079f9ab3b2
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-05-26 08:51:01 +0300

    Started with the Chat.js message logic and state

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Chat = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  const sendMessage = () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        time: new Date(Date.now()).getHours(),
+      };
+    }
+  };
   return (
     <div>
-      <div className="chat-header"></div>
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
       <div className="chat-body"></div>
       <div className="chat-footer"></div>
+      <input
+        type="text"
+        placeholder="Hey..."
+        onChange={(event) => {
+          setCurrentMessage(event.target.value);
+        }}
+      ></input>
+      <button>&#9658;</button>
     </div>
   );
 };
